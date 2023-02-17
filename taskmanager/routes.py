a914799790ddbd839b6668a10a3f50ec4b1b6ecc
rnc-personal <richard.challenger@protonmail.com>
2023-02-17 15:33:28 +0000

    BROKEN AGAIN

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -11,12 +11,21 @@ def home():
 @app.route("/categories")
 def categories():
     categories = list(Category.query.order_by(Category.category_name).all())
-     # This has been converted to a list from a cursor
-     # (we are navigating the db so its a cursor, list method converts it to a python list)
+    # This has been converted to a list from a cursor
+    # (we are navigating the db so its a cursor, list method converts it to a python list)
     return render_template("categories.html", categories=categories) 
     # 1st instance is passing the variable to the template.
     # The 2nd one is setting that variable to the ordered list created above
 
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
